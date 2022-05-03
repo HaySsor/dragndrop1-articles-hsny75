3ff0d39352528b125b000ad415d4e2ac9db2caf9
HaySsor <hayss007@gmail.com>
2022-05-03 20:14:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dragndrop1-articles-hsny75
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dragndrop1-articles-hsny75)
---
+++ index.js
@@ -0,0 +1,40 @@
+// Import stylesheets
+import './style.css';
+
+const containers = document.querySelectorAll('.box')
+const articles = document.querySelectorAll('.draggable')
+
+articles.forEach(article =>{
+  article.addEventListener('dragstart',()=>{
+    article.classList.add('is-dragged')
+  })
+  article.addEventListener('dragend',()=>{
+    article.classList.remove('is-dragged')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.is-dragged')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
